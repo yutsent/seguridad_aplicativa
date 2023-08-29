8fb5e9ad70ab884be689d7da684984e6c473da8f
Yutsent_seguridadaplicativa <43625345+yutsent@users.noreply.github.com>
2023-08-29 16:38:16 -0600

    Add files via upload

---
+++ dia2/actividad6.sh
@@ -0,0 +1,9 @@
+mkdir ~/herramientas
+mkdir ~/herramientas/deb
+mv ~/Downloads/code_1.81.1-1691620686_amd64.deb ~/herramientas/deb/
+mkdir ~/bak
+sudo cp ~/herramientas/deb/code_1.81.1-1691620686_amd64.deb ~/bak/code_1.81.1-1691620686_amd64.deb
+
+sudo dpkg -i /home/traine29/herramientas/deb/code_1.81.1-1691620686_amd64.deb
+
+figlet La instalacion termino correctamente
