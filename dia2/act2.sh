084382e0750491862ee1e0fce7472cc533788d6b
Yutsent_seguridadaplicativa <43625345+yutsent@users.noreply.github.com>
2023-08-29 12:39:30 -0600

    Update act2.sh
    cambio del directorio

--- dia2/act2.sh
+++ dia2/act2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo useradd -d /home/$1 -c "act2" -p '$1$TzvrEeVk$x/XxhCy7YYg9iJi6u6i.m/' -s /bin/zsh $1
+sudo useradd -m -c "act2" -p '$1$TzvrEeVk$x/XxhCy7YYg9iJi6u6i.m/' -s /bin/zsh $1
