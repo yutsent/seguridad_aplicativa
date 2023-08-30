12281b259f55d389066fa4acc276d3f036d21332
Yutsent_seguridadaplicativa <43625345+yutsent@users.noreply.github.com>
2023-08-30 11:01:56 -0600

    Update actividad1.py
    ahora compara frases

--- dia3/actividad1.py
+++ dia3/actividad1.py
@@ -1,7 +1,7 @@
 #crear un programa que detecte si una palabra es un palindromo
-nombre = input ("ingresa la palabra a verificar: ")
-
+nombre = input ("ingresa la cadena a verificar: ")
+nombre = nombre.replace(" ","")
 if nombre == nombre[::-1]:
-    print ("la palabra que ingresaste es un palindromo")
+    print ("la cadena que ingresaste es un palindromo")
 else:
-    print ("lo siento tu palabra no es un palindromo")
+    print ("lo siento tu frase no es un palindromo")
